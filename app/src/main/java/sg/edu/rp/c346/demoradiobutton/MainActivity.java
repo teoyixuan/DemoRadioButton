45411b50b50704f6d2ce8fa9f7d6ee3c4cd8a9db
18041430 <18041430@myrp.edu.sg>
2020-04-23 12:33:13 +0800

    done

--- app/src/main/java/sg/edu/rp/c346/demoradiobutton/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/demoradiobutton/MainActivity.java
@@ -3,12 +3,32 @@ package sg.edu.rp.c346.demoradiobutton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    RadioGroup rg;
+    RadioButton rb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn =(Button) findViewById(R.id.button1);
+        Log.d("qweqweqwe", "onClick: hi");
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                rg = findViewById(R.id.radioGroup1);
+                int selectedButtonId = rg.getCheckedRadioButtonId();
+                rb = findViewById(selectedButtonId);
+                Toast.makeText(MainActivity.this, rb.getText(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
